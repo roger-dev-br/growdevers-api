45733648bc5ed1c1dbaa68c58316ddbdb71b0941
pviniciusm <pvinics@gmail.com>
2022-12-03 17:40:33 -0300

    add: cache para a listagem de growdevers

--- src/app/features/growdever/usecases/list-growdevers.usecase.ts
+++ src/app/features/growdever/usecases/list-growdevers.usecase.ts
@@ -1,11 +1,36 @@
+import { CacheRepository } from "../../../shared/database/repositories/cache.repository";
 import { GrowdeverRepository } from "../repositories/growdever.repository";
 
+/**
+ * Lista todos os growdevers da base de dados.
+ * Se a lista está em cache, então retorna o cache.
+ */
 export class ListGrowdeversUseCase {
-    constructor(private repository: GrowdeverRepository) {}
+    /** Chave onde a lista de growdevers é salva no cache do Redis */
+    private cacheKey = "growdevers";
+
+    constructor(
+        private repository: GrowdeverRepository,
+        private cacheRepository: CacheRepository
+    ) {}
 
     public async execute() {
-        const result = await this.repository.list();
+        // 1 - Busca a lista no cache (Redis)
+        const cached = await this.cacheRepository.get(this.cacheKey);
 
-        return result.map((item) => item.toJson());
+        // Se há cache, retorna o valor que foi salvo
+        if (cached) {
+            return cached;
+        }
+
+        // 2 - Se não há cache, vai até o repo do Postgres
+        const growdevers = await this.repository.list();
+        const result = growdevers.map((item) => item.toJson());
+
+        // 3 - Salva o resultado em cache para não precisar
+        // consultar o Postgres novamente
+        await this.cacheRepository.set(this.cacheKey, result);
+
+        return result;
     }
 }
