ca5046410acd98a2d86ff7ebcfc845a55dc22f7c
pviniciusm <pvinics@gmail.com>
2022-12-03 12:26:53 -0300

    add redisconnection file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { DatabaseConnection } from "./main/database";
+import { RedisConnection } from "./main/database/redis.connection";
 import { runServer } from "./main/server";
 
-import { exampleRedis } from "./examples/redis/redis.example";
-
-DatabaseConnection.connect().then(() => {
-    runServer();
-    exampleRedis();
-});
+DatabaseConnection.connect().then(RedisConnection.connect).then(runServer);
