d5ee62f74a2467d377d1d6a7a842fa1e35617a09
pviniciusm <pvinics@gmail.com>
2022-12-04 10:23:53 -0300

    fix: bug na inserção de skills

--- src/app/features/skills/usecases/add-skill.usecase.ts
+++ src/app/features/skills/usecases/add-skill.usecase.ts
@@ -9,9 +9,17 @@ export class AddSkillUseCase {
     constructor(private repository: GrowdeverRepository) {}
 
     public async execute(data: AddSkillDTO) {
+        const growdever = await this.repository.get(data.id);
+
+        if (!growdever) {
+            return null;
+        }
+
+        const skills = growdever.skills;
+
         const result = await this.repository.update({
             id: data.id,
-            skills: [data.skill],
+            skills: [...skills, data.skill],
         });
 
         return result?.toJson();
