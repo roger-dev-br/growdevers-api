15e26b68f53c37dbed6fd317df0c71415fa53033
pviniciusm <pvinics@gmail.com>
2022-12-12 23:54:54 -0300

    config for integration tests

--- src/main/config/database.config.ts
+++ src/main/config/database.config.ts
@@ -1,7 +1,7 @@
 import { DataSource } from "typeorm";
 import "dotenv/config";
 
-export default new DataSource({
+let dataSource = new DataSource({
     type: "postgres",
     url: process.env.DB_URL,
     ssl: {
@@ -12,3 +12,15 @@ export default new DataSource({
     migrations: ["src/app/shared/database/migrations/**/*.ts"],
     schema: "aula",
 });
+
+if (process.env.NODE_ENV === "test") {
+    dataSource = new DataSource({
+        type: "sqlite",
+        database: "database.sqlite3",
+        synchronize: false,
+        entities: ["src/app/shared/database/entities/**/*.ts"],
+        migrations: ["tests/shared/migrations/**/*.ts"],
+    });
+}
+
+export default dataSource;
